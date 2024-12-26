b57f78d61c3ad0ee824e7e1ed177f93009c10b8f
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-12-26 17:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-var isDate = function (input) {
-  const regex = /^\d{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[1-2]\d|3[0-1])$/;
-  return regex.test(input); // Directly return the result of the regex test
-};
+function isDate(input) {
+  // Check if the input is a valid Date object
+  if (input instanceof Date && !isNaN(input)) {
+    return true;
+  }
+
+  // Check if the input is a string or number that can be parsed into a valid Date
+  if (typeof input === "string" || typeof input === "number") {
+    const parsedDate = new Date(input);
+    return !isNaN(parsedDate);
+  }
+
+  // For all other types, return false
+  return false;
+}
 
 // Do not change the code below.
 const input = prompt("Enter Date.");
